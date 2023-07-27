1ade00b081349ecdf44be1cc602f2dbb1b879b3c
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-28 01:33:06 +0300

    upd filtr

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -4,14 +4,13 @@ export const Description = styled.p`
   font-weight: bold;
   font-size: 18px;
   margin-top: 20px;
-  color: rgb(12 80 206);
 `;
 
 export const Input = styled.input`
   width: 550px;
   padding: 4px;
   margin-top: 6px;
-  border: 1px solid rgb(12 80 206);
+  border: 1px solid rgb(0 0 0);
   border-radius: 4px;
   &:focus {
     border-color: rgb(99, 149, 241, 0.6);
