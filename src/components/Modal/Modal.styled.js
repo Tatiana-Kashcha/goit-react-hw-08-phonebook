5fcbfe0e5cc60946010a69e9b9cf2ae789429691
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-28 01:36:39 +0300

    Update Modal.styled.js

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -16,7 +16,7 @@ export const Overlay = styled.div`
 export const Modal = styled.div`
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
-  background-color: ffff;
+  background-color: #ffff;
 `;
 
 export const CloseButton = styled.button`
