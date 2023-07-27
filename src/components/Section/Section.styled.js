2420cf725460c338af43dc33fe1a211f903297a1
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-27 17:03:23 +0300

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -4,6 +4,8 @@ export const Section = styled.section`
   max-width: 800px;
   margin-left: auto;
   margin-right: auto;
+  border: 1px solid;
+  border-color: rgb(156 72 35);
 `;
 
 export const Container = styled.div`
@@ -15,4 +17,5 @@ export const SectionTitle = styled.h2`
   font-size: 32px;
   padding-top: 20px;
   margin-bottom: 20px;
+  text-align: center;
 `;
